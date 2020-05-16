7ab1f1b460bac4ec91008187647236f1ec7a2ded
Berat Bozkurt <beratbozkurt1999@gmail.com>
2020-05-16 03:42:07 +0300

    initial commit and add header component

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <app-header />
+    {{ msg }} hello
+  </div>
+</template>
+
+<script>
+import AppHeader from "./components/header/header";
+
+export default {
+  name: "app",
+  components: { AppHeader },
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<style>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand active" href="/home" aria-current="page"
+        >vue-practical</a
+      ><button class="navbar-toggler" type="button">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div
+        class="justify-content-end navbar-collapse"
+        id="navbarSupportedContent"
+        style="display: block;"
+      >
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item">
+            <a class="nav-link active" href="/home" aria-current="page">Home</a>
+          </li>
+          <li class="nav-item" style="display: none;">
+            <a class="nav-link" href="/profile/null">Profil</a>
+          </li>
+          <li class="nav-item" style="display: inherit;">
+            <a class="nav-link" href="/home" aria-current="page">Login</a>
+          </li>
+          <li class="nav-item" style="display: inherit;">
+            <a class="nav-link" href="/sign-up">Register</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
